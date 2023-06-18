a381786f3e876a671610657dd6a517b287db1bfd
Sergii <sergiyomelchuk@outlook.com>
2023-06-18 16:19:56 +0300

    Task update - Notify

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -37,7 +37,7 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] < new Date()) {
-      window.alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
       return;
     }
     refs.startBtnEl.removeAttribute('disabled');
