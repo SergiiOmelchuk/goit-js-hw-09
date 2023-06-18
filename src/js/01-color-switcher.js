33633052cae9c5cf927234d4d6a63c010d187154
Sergii <sergiyomelchuk@outlook.com>
2023-06-18 13:08:36 +0300

    update Task-1 (js 01)
    Код працює

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,47 +4,23 @@ const refs = {
   stopBtn: document.querySelector('button[data-stop]'),
 };
 
-let timeIntervalId = null;
-refs.stopBtn.disabled = true;
+let timerId = null;
+refs.startBtn.addEventListener('click', changeBgColor);
+function changeBgColor() {
+  timerId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  refs.startBtn.disabled = true;
+}
 
-const randomColorGen = {
-  DELAY: 1000,
+refs.stopBtn.addEventListener('click', stopChangeBgColor);
+function stopChangeBgColor() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+}
 
-  getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
-  },
-
-  timeInterval() {
-    timeIntervalId = setInterval(() => {
-      randomColorGen.changeBgColor();
-      console.log(randomColorGen.changeBgColor());
-    }, randomColorGen.DELAY);
-    refs.stopBtn.disabled = false;
-    console.log(`set interval func`);
-  },
-
-  start() {
-    refs.startBtn.addEventListener('click', () => {
-      randomColorGen.timeInterval();
-      refs.startBtn.disabled = true;
-      refs.stopBtn.disabled = false;
-      console.log(`start function`);
-    });
-    refs.startBtn.addEventListener('click', randomColorGen.stop);
-  },
-
-  stop() {
-    clearInterval(timeIntervalId);
-    refs.startBtn.disabled = false;
-    refs.stopBtn.disabled = true;
-  },
-
-  changeBgColor() {
-    refs.bodyEl.style.backgroundColor = `${randomColorGen.getRandomHexColor()}`;
-    console.log(`change bg func`);
-  },
-};
-
-randomColorGen.start();
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
